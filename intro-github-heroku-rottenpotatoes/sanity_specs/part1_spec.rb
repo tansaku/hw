44f25fef3ade5baa2f2db465bbbf842957ba5753
Sam Joseph <tansaku@gmail.com>
2013-06-28 18:06:17 +0100

    oops

--- intro-github-heroku-rottenpotatoes/sanity_specs/part1_spec.rb
+++ intro-github-heroku-rottenpotatoes/sanity_specs/part1_spec.rb
@@ -4,10 +4,3 @@ describe "Stack" do
     it "should define #{method}" { Stack.new('a',1).should respond_to method }
   end
 end
-
-describe "dessert" do
-  it "should define a constructor" { lambda { Dessert.new }.should_not raise_error(::NoMethodError) }
-  %w(healthy? delicious? lowfat?).each do |method|
-    it "should define #{method}" { Dessert.new('a',1).should respond_to method }
-  end
-end
