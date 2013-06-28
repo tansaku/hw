185b1e7dd6f169ed23d6b7c11e7cb30a2ca88285
Sam Joseph <tansaku@gmail.com>
2013-06-28 18:15:35 +0100

    aha, switching curly braces to do/end appears to have fixed it

--- intro-github-heroku-rottenpotatoes/sanity_specs/part1_spec.rb
+++ intro-github-heroku-rottenpotatoes/sanity_specs/part1_spec.rb
@@ -1,6 +1,10 @@
 describe "Stack" do
-  it "should define a constructor" { lambda { Stack.new }.should_not raise_error(::NoMethodError) }
+  it "should define a constructor" do
+    lambda { Stack.new }.should_not raise_error(::NoMethodError)
+  end
   %w(pop? push? clear?).each do |method|
-    it "should define #{method}" { Stack.new('a',1).should respond_to method }
+    it "should define #{method}" do
+      Stack.new('a',1).should respond_to method
+    end
   end
 end
--- ruby-calisthenics/sanity_specs/part4_spec.rb
+++ ruby-calisthenics/sanity_specs/part4_spec.rb
@@ -1,13 +1,21 @@
 describe "dessert" do
-  it "should define a constructor" { lambda { Dessert.new }.should_not raise_error(::NoMethodError) }
+  it "should define a constructor" do
+    lambda { Dessert.new }.should_not raise_error(::NoMethodError)
+  end
   %w(healthy? delicious? lowfat?).each do |method|
-    it "should define #{method}" { Dessert.new('a',1).should respond_to method }
+    it "should define #{method}" do
+      Dessert.new('a',1).should respond_to method
+    end
   end
 end
 
 describe "jellybean" do
-  it "should define a constructor" { lambda { JellyBean.new }.should_not raise_error(::NoMethodError) }
+  it "should define a constructor" do
+    lambda { JellyBean.new }.should_not raise_error(::NoMethodError)
+  end
   %w(healthy? delicious? lowfat?).each do |method|
-    it "should define #{method}" { JellyBean.new('a',1).should respond_to method }
+    it "should define #{method}" do
+      JellyBean.new('a',1).should respond_to method
+    end
   end
 end
