df6780ae821c66396385fe0bab3774530bf9b8b7
Armando Fox <fox@cs.berkeley.edu>
2013-08-20 18:00:41 -0700

    working version for testing

--- oracle-of-bacon/public/lib/oracle_of_bacon.rb
+++ oracle-of-bacon/public/lib/oracle_of_bacon.rb
@@ -21,76 +21,52 @@ class OracleOfBacon
 
   def from_does_not_equal_to
     # YOUR CODE HERE
-    if @from == @to
-      self.errors.add(:from, 'cannot be the same as To')
-    end
   end
 
   def initialize(api_key='')
-    @api_key = api_key
-    @from = 'Kevin Bacon'
-    @to = 'Kevin Bacon'
-    @uri = nil
+    # your code here
   end
 
   def find_connections
-    raise InvalidError unless self.valid?
     make_uri_from_arguments
     begin
-      xml = URI.parse(@uri).read
+      xml = URI.parse(uri).read
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
       Net::ProtocolError => e
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
-      raise NetworkError, e.message
+      # your code here
     end
-    @response = Response.new(xml)
+    # your code here: create the OracleOfBacon::Response object
   end
 
   def make_uri_from_arguments
-    @uri =
-      'http://oracleofbacon.org/cgi-bin/xml?p=' +
-      CGI.escape(api_key) +
-      '&a=' + CGI.escape(from) +
-      '&b=' + CGI.escape(to)
+    # your code here: set the @uri attribute to properly-escaped URI
+    #   constructed from the @from, @to, @api_key arguments
   end
       
   class Response
     attr_reader :type, :data
+    # create a Response object from a string of XML markup.
     def initialize(xml)
       @doc = Nokogiri::XML(xml)
       parse_response
     end
+
     private
+
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      elsif ! @doc.xpath('/link').empty?
-        parse_graph_response
-      elsif ! @doc.xpath('/spellcheck').empty?
-        parse_spellcheck_response
-      else
-        parse_unknown_response
+      # your code here: 'elsif' clauses to handle other responses
+      # for responses not matching the 3 basic types, the Response
+      # object should have type 'unknown' and data 'unknown response'         
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
-    end
-    def parse_graph_response
-      @type = :graph
-      actors = @doc.xpath('//actor')
-      movies = @doc.xpath('//movie')
-      @data = actors.zip(movies).flatten.compact.map(&:text)
-    end
-    def parse_unknown_response
-      @type = :unknown
-      @data = 'Unknown response type'
-    end
-    def parse_spellcheck_response
-      @type = :spellcheck
-      @data = @doc.xpath('//match').map(&:text)
     end
   end
 end
